9ee8b24ffd98dfded21dbd1758471b56ea2fd9fa
paramGulhane <63253706+paramGulhane@users.noreply.github.com>
2020-07-20 11:24:46 +0530

    Add files via upload

---
+++ JS day4/scripts/q7.js
@@ -0,0 +1,9 @@
+let n = Number(prompt("Enter a Number"));
+console.log(`Prime numbers till ${n}: \n`);
+nextPrime:
+for(let i=2;i<=n;i++){
+    for(let j=2;j<i;j++){
+        if(i%j==0) continue nextPrime;
+    }
+    console.log(i);
+}
